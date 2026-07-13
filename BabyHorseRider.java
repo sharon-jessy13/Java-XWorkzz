a4ac7797233835d83e323256323a207001b7be0f
sharon <sharonjessyts@gmail.com>
2026-07-13 13:24:03 +0530

    examples of variables and data type

---
+++ BabyHorseRider.java
@@ -0,0 +1,29 @@
+class BabyHorseRider {
+	static String material = "Plastic";
+    static double price = 682;
+    static String productDimensions = "10*5*10"; 
+    static String genericName = "Baby Care";
+    static String includedComponents = "10*5*10";
+    static String countryOfOrigin = "India";
+    static String productSafetyInformation = "Product may be regulated by BIS Quality Control Order. Visit https://www.bis.gov.in/know-your-standard/ for more information; check IS mark on item/ upon delivery.";
+    static String manufacturerInformation = "KIDDYKIN, DELHI Pream Nagar, Kirari, Delhi 110086";
+    static String importerInformation = "No information available";
+    static String packerInformation = "KIDDYKIN, DELHI Pream Nagar, Kirari, Delhi 110086";
+    static int netWeightGrams = 350;
+    static String supplierInformation = "BALAJI_SALES c/o Meesho";
+        
+    public static void main(String horse[]) {
+        System.out.println("Material is : " + material);
+		System.out.println("price :" + price);
+        System.out.println("Product Dimensions is : " + productDimensions);
+        System.out.println("Generic Name is : " + genericName);
+        System.out.println("Included Components is : " + includedComponents);
+        System.out.println("Country of Origin is : " + countryOfOrigin);
+        System.out.println("Product Safety Information is : " + productSafetyInformation);
+        System.out.println("Manufacturer Information is : " + manufacturerInformation);
+        System.out.println("Importer Information is : " + importerInformation);
+        System.out.println("Packer Information is : " + packerInformation);
+        System.out.println("Net Weight(g) is : " + netWeightGrams);
+        System.out.println("Supplier Information is : " + supplierInformation);
+    }
+}
---
+++ Shrug.java
@@ -0,0 +1,45 @@
+class Shrug {
+	static String color = "White";
+	static String fabric = "Cotton Blend";
+	static String fitShape = "Shrug";
+	static String length = "Crop";
+	static String neckCollar = "Open Front";
+	static String sleeveLength = "Long Sleeves";
+	static String sleeveStyling = "Regular Sleeves";
+	static String printOrPatternType = "Solid";
+	static String pattern = "Solid";
+	static String surfaceStyling = "Tie-Ups";
+	static int netQuantity = 2;
+	static String genericName = "Topwear";
+	static String countryOfOrigin = "India";
+	static String manufacturerInformation = "Shree Khatu Shyam Traders 108 House no-4 3rd Floor 121004";
+	static String importerInformation = "No information available";
+	static String packerInformation = "Shree Khatu Shyam Traders 108 House no-4 3rd Floor 121004";
+	static int netWeightGrams = 185;
+	static String supplierInformation = "SHREE KHATU SHYAM TRADERS 108 c/o Meesho";
+	static String contactInformation = "Meesho operates as a marketplace wherein third-party sellers engage in the sale of products to customers. To contact a seller, kindly dispatch a written communication to the address provided below including the product page URL. To, Seller Name - SHREE KHATU SHYAM TRADERS 108 PID - 509931055 Seller Mailbox: Contact Seller 3rd Floor, Wing-E, Helios Business Park, Kadubeesanahalli Village, Varthur Hobli, Outer Ring Road, Bengaluru, Karnataka 560103";
+    static String legalDisclaimer = "All images are indicative. Prior to use, please read product packaging for complete details, including manufacturing, shelf life/expiry, usage, dosage, storage instructions and conditions. Product information is based on supplier data. Keep out of reach of children, where advised.";
+
+    public static void main(String shrug[]) {
+        System.out.println("color is : " + color);
+        System.out.println("fabric is : " + fabric);
+        System.out.println("fit/ shape is : " + fitShape);
+        System.out.println("length is : " + length);
+        System.out.println("neck/ collar is : " + neckCollar);
+        System.out.println("sleeve length is : " + sleeveLength);
+        System.out.println("sleeve styling is : " + sleeveStyling);
+        System.out.println("print or pattern type is : " + printOrPatternType);
+        System.out.println("pattern is : " + pattern);
+        System.out.println("surface styling is : " + surfaceStyling);
+        System.out.println("net quantity is : " + netQuantity);
+        System.out.println("generic name is : " + genericName);
+        System.out.println("country of origin is : " + countryOfOrigin);
+        System.out.println("manufacturer information is : " + manufacturerInformation);
+        System.out.println("importer information is : " + importerInformation);
+        System.out.println("packer information is : " + packerInformation);
+        System.out.println("net weight(g) is : " + netWeightGrams);
+        System.out.println("supplier information is : " + supplierInformation);
+        System.out.println("contact information is : " + contactInformation);
+        System.out.println("legal disclaimer is : " + legalDisclaimer);
+    }
+}
