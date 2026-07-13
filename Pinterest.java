c08dabbe6d185f497f9b80cb60f72d887d1adb3c
sharon <sharonjessyts@gmail.com>
2026-07-13 21:55:37 +0530

    Separate main and logic classes

---
+++ Pinterest.java
@@ -0,0 +1,15 @@
+class Pinterest{
+	
+	public static void createAccount(String email, String passward, String birthday){
+		
+		System.out.println("method started");
+		
+		
+		System.out.println("email is :" + email);
+		System.out.println("passward is :" + passward);
+		System.out.println("birthday is : "+ birthday);
+		
+		System.out.println("method ended");
+		
+	}
+}
---
+++ PinterestExecutor.java
@@ -0,0 +1,12 @@
+class PinterestExecutor {
+	
+	public static void main(String printrest[]){
+		
+		System.out.println("main Started");
+		
+		Pinterest.createAccount("sharon@gmail.com", "Sharon@123", "13/11/2004");
+		
+		System.out.println("main ended");
+		
+	}
+}
